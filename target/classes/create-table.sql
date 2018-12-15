2b79b78517d61a6f737d1f8feaa7aa3429814537
KateV <katyaveremchuk7@gmail.com>
2018-12-15 22:42:58 +0300

    First commit

--- target/classes/create-table.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS employee;
-CREATE TABLE employee (
-  employee_id SERIAL NOT NULL,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  department_id INTEGER NOT NULL,
-  job_title VARCHAR(50) NOT NULL,
-  gender gender,
-  date_of_birth date);
