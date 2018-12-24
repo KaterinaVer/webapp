584bd7b619a4d23fc8c759dca6d4f1f43fb243d9
KateV <katyaveremchuk7@gmail.com>
2018-12-24 14:28:11 +0300

    dao

--- target/classes/create-table.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS employee;
-CREATE TABLE employee (
-  employee_id SERIAL NOT NULL,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  department_id INTEGER NOT NULL,
-  job_title VARCHAR(50) NOT NULL,
-  gender gender NOT NULL,
-  date_of_birth date NOT NULL);
