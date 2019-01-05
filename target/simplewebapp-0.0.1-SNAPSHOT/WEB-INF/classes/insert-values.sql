d3279022b6955f3295aa9d6188ba71e69bf5fa13
KateV <katyaveremchuk7@gmail.com>
2019-01-05 23:17:32 +0300

    added controller

--- target/simplewebapp-0.0.1-SNAPSHOT/WEB-INF/classes/insert-values.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO employee (first_name,last_name,department_id,job_title,gender,date_of_birth)
-VALUES ('Archie', 'Nore'  , 23, 'Manager'  , 'MALE',   '1975-06-10'),
-       ('Betty',  'Cooper', 23, 'Secretary', 'FEMALE', '1980-05-04');
